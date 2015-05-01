0c8bba4375c4cc9a21e0de9cca07f13d5df13304
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:18:24 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var reInflect = require("re_inflect");
 
 
-module.exports = function dasherize(string) {
+module.exports = dasherize;
+
+
+function dasherize(string) {
     return string.match(reInflect).join("-").toLowerCase();
-};
+}
